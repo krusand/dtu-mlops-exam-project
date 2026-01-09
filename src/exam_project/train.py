df01773a14bfc9350e31e1e302dd7db66c3f97c4
krusand <110490348+krusand@users.noreply.github.com>
2026-01-09 12:57:23 +0100

    chore: add uv

--- src/exam_project/train.py
+++ src/exam_project/train.py
@@ -1,10 +1,34 @@
 from exam_project.model import Model
 from exam_project.data import MyDataset
+from pytorch_lightning import LightningModule, Trainer
+from torch import nn, optim
+import torch
+import transformers
 
+def get_trainer(model, trainer_args):
+    """
+    Gets a Trainer object of either transformers or pytorch lightning
+
+    params:
+        model: The model class
+        trainer_args: Training arguments
+    """
+
+    if model is "lightning":
+        trainer = Trainer(**trainer_args)
+    elif model is "huggingface":
+        trainer = transformers.Trainer(**trainer_args)
+
+    return trainer
+    
 def train():
-    dataset = MyDataset("data/raw")
+    train, val, test = MyDataset("data/raw")
     model = Model()
+    trainer = get_trainer()
+    trainer.fit(model=model)
     # add rest of your training code here
 
+
 if __name__ == "__main__":
+
     train()
