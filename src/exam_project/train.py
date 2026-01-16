24dff7a87f56256918421f7493ef33975034922b
krusand <110490348+krusand@users.noreply.github.com>
2026-01-16 11:21:21 +0100

    working config

--- src/exam_project/train.py
+++ src/exam_project/train.py
@@ -9,12 +9,7 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 import torch
 import transformers
 from typing import Annotated
-from dotenv import load_dotenv
-import os
-load_dotenv()
-api_key = os.getenv("WANDB_API_KEY")
-entity = os.getenv("WANDB_ENTITY")
-project = os.getenv("WANDB_PROJECT")
+
 
 @hydra.main(config_path="configs", config_name="train", version_base=None)
 def train(cfg):
@@ -32,21 +27,20 @@ def train(cfg):
     )
 
     trainer_args = {"max_epochs": cfg.trainer.max_epochs
-                    ,'limit_train_batches': 0.05
                     , 'accelerator': cfg.trainer.accelerator
-                    , 'logger': instantiate(cfg.logger.wandb)
+                    , 'logger': WandbLogger(log_model=True, project=cfg.logger.wandb.project)
                     , 'log_every_n_steps': 5
                     , "callbacks": [checkpoint_callback]}
     
     train, val, test = load_data(processed_dir='data/processed/')
-    train = torch.utils.data.DataLoader(train, persistent_workers=True, num_workers=9, batch_size=cfg.trainer.batch_size)
-    val = torch.utils.data.DataLoader(val, persistent_workers=True, num_workers=9, batch_size=cfg.trainer.batch_size)
-    test = torch.utils.data.DataLoader(test, persistent_workers=True, num_workers=9, batch_size=trainer.batch_size)
+    train = torch.utils.data.DataLoader(train, persistent_workers=True, num_workers=9, batch_size=cfg.data.batch_size)
+    val = torch.utils.data.DataLoader(val, persistent_workers=True, num_workers=9, batch_size=cfg.data.batch_size)
+    test = torch.utils.data.DataLoader(test, persistent_workers=True, num_workers=9, batch_size=cfg.data.batch_size)
     
     model = instantiate(cfg.models)
-    trainer = instantiate(cfg.trainer)
+    trainer = Trainer(**trainer_args)
     trainer.fit(model=model, train_dataloaders=train, val_dataloaders=val)
-    print(checkpoint_callback.best_model_path)
+
 
 if __name__ == "__main__":
     train()
