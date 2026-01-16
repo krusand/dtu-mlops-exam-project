4a91e9e4726547d68194a2a84f18b038840f564d
krusand <110490348+krusand@users.noreply.github.com>
2026-01-16 14:48:07 +0100

    pr fix

--- src/exam_project/model.py
+++ src/exam_project/model.py
@@ -181,6 +181,7 @@ class ViTClassifier(LightningModule):
         output_dim: int,
     ) -> None:
         super().__init__()
+        self.model_name = model_name
         self.lr = lr
         self.freeze_backbone = freeze_backbone
         self. output_dim = output_dim
