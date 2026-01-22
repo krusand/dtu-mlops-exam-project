b91dae8800401c14ec15c070fd56e1813c095b76
krusand <110490348+krusand@users.noreply.github.com>
2026-01-22 21:11:49 +0100

    chore: logging, code improvements

--- src/exam_project/pre_productionize_model.py
+++ src/exam_project/pre_productionize_model.py
@@ -1,19 +1,21 @@
 import typer
 import os
 import wandb
+from loguru import logger
 
 from dotenv import load_dotenv
 load_dotenv()
 
 def productionize_artifact(artifact_path: str) -> None:
     """
-    Promotes model to production alias
+    Promotes model to 'production' alias
 
     Args:
         artifact_path: Path to the artifact to stage.
             Should be of the format "entity/project/artifact_name:version".
     """
-    
+    logger.info("Productionizing artifact")
+
     api = wandb.Api(
         api_key=os.getenv("WANDB_API_KEY"),
         overrides={"entity": os.getenv("WANDB_ENTITY_ORG")
@@ -21,22 +23,35 @@ def productionize_artifact(artifact_path: str) -> None:
     )
 
     wandb_entity, wandb_registry, artifact_name_version = artifact_path.split("/")
+    
+    logger.info(f"{wandb_entity}")
+    logger.info(f"{wandb_registry}")
+    logger.info(f"{artifact_name_version}")
 
     artifact_name, artifact_version = artifact_name_version.split(":")
+
+    logger.info(f"{artifact_name}")
+    logger.info(f"{artifact_version}")
+
     artifact = api.artifact(artifact_path)
     current_aliases = set(artifact.aliases)
 
+    logger.info(f"{current_aliases = }")
+
     if 'production' in current_aliases:
-        typer.echo(f"Info: {artifact.name} is already tagged with 'production'")
+        logger.info(f"Info: {artifact.name} is already tagged with 'production'")
     else:
         current_aliases.add('production')
-        typer.echo(f"Promoting {artifact.name} to 'production'")
+        logger.info(f"Promoting {artifact.name} to 'production'")
 
     if 'staging' in current_aliases:
         current_aliases.remove('staging')
-        typer.echo(f"Removing 'staging' tag from {artifact.name}")
+        logger.info(f"Removing 'staging' tag from {artifact.name}")
 
     artifact.aliases=list(current_aliases)
+
+    logger.info(f"New model aliases: {list(current_aliases)}")
+
     artifact.save()
 
 if __name__ == "__main__":
--- src/exam_project/stage_model_check.py
+++ src/exam_project/stage_model_check.py
@@ -8,9 +8,9 @@ import time
 import torch
 from sklearn.metrics import accuracy_score
 from dotenv import load_dotenv
+from loguru import logger
+
 load_dotenv()
-
-
 
 def test_model_speed() -> bool:
     """
@@ -19,13 +19,18 @@ def test_model_speed() -> bool:
     Returns:
         should_promote (bool): True if can predict >100 pr. second, else False
     """
-
+    logger.info("Starting testing model speed")
     staging_model, staging_artifact = load_model_from_wandb(artifact=os.getenv("MODEL_NAME"), alias='staging')
     start = time.time()
+    logger.info(f"{start = }")
     for _ in range(100):
         staging_model(torch.rand(1, 1, 48, 48).to(get_device_from_artifact(staging_artifact)))
     end = time.time()
     should_promote = end - start < 1
+    logger.info(f"{end = }")
+    logger.info(f"Time diff: {(end-start)}")
+    logger.info(f"{should_promote = }")
+    logger.info("Finished testing model speed")
     return should_promote
 
 def evaluate_model(model: BaseANN | BaseCNN | ViTClassifier, test_dataloader: torch.utils.data.DataLoader, device: str) -> float:
@@ -40,6 +45,7 @@ def evaluate_model(model: BaseANN | BaseCNN | ViTClassifier, test_dataloader: to
     Returns:
         - test_acc (float): The test accuracy of the model
     """
+    logger.info("Evaluating model")
 
     y_pred = []
     y_true = []
@@ -55,7 +61,13 @@ def evaluate_model(model: BaseANN | BaseCNN | ViTClassifier, test_dataloader: to
     y_true = torch.cat(y_true).to(device)
     y_pred = torch.cat(y_pred).to(device)
 
+    logger.info(f"Number of predictions: {len(y_pred.tolist())}")
+    
     test_acc = accuracy_score(y_true.tolist(), y_pred.tolist())
+    
+    logger.info(f"{test_acc = }")
+    logger.info("Finished evaluating model")
+    
     return test_acc
 
 
@@ -66,31 +78,38 @@ def test_staging_against_production_model() -> bool:
     Returns:
         - should_promote (bool): True if staging accuracy is better than production accuracy
     """
-
+    logger.info("Testing staging model against production model")
     staging_model, staging_artifact = load_model_from_wandb(os.getenv("MODEL_NAME"), alias='staging')
     production_model, production_artifact = load_model_from_wandb(os.getenv("MODEL_NAME"), alias='production')
     _, _, test = load_data(processed_dir="data/processed/")
     test = torch.utils.data.DataLoader(test, batch_size=64)
 
-
     staging_accuracy = evaluate_model(staging_model, test, get_device_from_artifact(staging_artifact))
     production_accuracy = evaluate_model(production_model, test, get_device_from_artifact(production_artifact))
-    print(f"{staging_accuracy = }")
-    print(f"{production_accuracy = }")
     should_promote = staging_accuracy > production_accuracy
+
+    logger.info(f"{staging_accuracy = }")
+    logger.info(f"{production_accuracy = }")
+    logger.info(f"{should_promote = }")
+    logger.info("Finished testing staging model against production model")
     return should_promote
     
 def main():
+    logger.info("Stage and checking model")
+
     validate_environment()
     should_promote = all([test_staging_against_production_model()])
     should_promote = 'true' if should_promote else 'false' # Used for better yaml handling
         
     if "GITHUB_OUTPUT" in os.environ:
+        logger.info("GITHUB_OUTPUT in environment, writing to it")
         with open(os.environ["GITHUB_OUTPUT"], "a") as f:
             print(f"promote={should_promote}", file=f)
     else:
         # Fallback for local testing
-        print(f"Not in CI/CD. Output would be: promote={should_promote}")
+        logger.info(f"Not in CI/CD. Output would be: promote={should_promote}")
+    
+    logger.info("Stage and checking model finished")
 
 
 if __name__ == '__main__':
